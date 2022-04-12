1692b26574ec4764bc8652f84c1a98641dc61912
James Dunkerley <james.dunkerley@gmail.com>
2022-04-12 11:12:12 +0100

    Sum speed up

--- std-bits/table/src/main/java/org/enso/table/aggregations/Sum.java
+++ std-bits/table/src/main/java/org/enso/table/aggregations/Sum.java
@@ -1,9 +1,12 @@
 package org.enso.table.aggregations;
 
+import org.enso.table.data.column.storage.DoubleStorage;
+import org.enso.table.data.column.storage.LongStorage;
 import org.enso.table.data.column.storage.Storage;
 import org.enso.table.data.table.Column;
 import org.enso.table.data.table.problems.InvalidAggregation;
 
+import java.util.BitSet;
 import java.util.List;
 
 /***
@@ -19,8 +22,20 @@ public class Sum extends Aggregator {
 
   @Override
   public Object aggregate(List<Integer> indexes) {
+    if (storage instanceof LongStorage) {
+      return aggregateLong((LongStorage)storage, indexes);
+    }
+
+    if (storage instanceof DoubleStorage) {
+      return aggregateDouble((DoubleStorage)storage, indexes);
+    }
+
+    return aggregateObject(indexes);
+  }
+
+  private Object aggregateObject(List<Integer> indexes) {
     Object current = null;
-    for (int row: indexes) {
+    for (int row : indexes) {
       Object value = storage.getItemBoxed(row);
       if (value != null) {
         if (current == null) {
@@ -44,5 +59,37 @@ public class Sum extends Aggregator {
       }
     }
     return current;
+  }
+
+  private Long aggregateLong(LongStorage longStorage, List<Integer> indexes) {
+    BitSet isMissing = longStorage.getIsMissing();
+
+    boolean any = false;
+    long total = 0L;
+
+    for (int row : indexes) {
+      if (!isMissing.get(row)) {
+        total += longStorage.getItem(row);
+        any = true;
+      }
+    }
+
+    return any ? total : null;
+  }
+
+  private Double aggregateDouble(DoubleStorage doubleStorage, List<Integer> indexes) {
+    BitSet isMissing = doubleStorage.getIsMissing();
+
+    boolean any = false;
+    double total = 0.0;
+
+    for (int row : indexes) {
+      if (!isMissing.get(row)) {
+        total += doubleStorage.getItem(row);
+        any = true;
+      }
+    }
+
+    return any ? total : null;
   }
 }
