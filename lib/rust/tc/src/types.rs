013dde61ee38c3f505b506ba3561ed7917992378
Edward Kmett <ekmett@gmail.com>
2022-11-28 01:07:18 -0500

    start trying a version of this where we use Box<>
    this will be suboptimal in many ways but box patterns would
    let me express a lot of algorithms easily, and i need a higher
    productivity way to work

---
+++ lib/rust/tc/src/types.rs
@@ -0,0 +1,231 @@
+use std::iter::Iterator;
+use crate::bwd::Bwd;
+use crate::fwd::Fwd;
+
+#[derive(PartialEq,Eq,PartialOrd,Ord,Copy,Clone,Hash,Debug)]
+pub struct TyName(u32);
+
+// we're going to do too much copying for now because box patterns are too useful
+#[derive(PartialEq,Eq,PartialOrd,Ord,Clone,Hash,Debug)]
+pub enum Ty {
+  V(TyName),
+  Arr(Box<Ty>,Box<Ty>) // i hate everything about this
+}
+
+pub fn v(n: TyName) -> Ty {
+  Ty::V(n)
+}
+
+pub fn arr(arg: Ty, res: Ty) -> Ty {
+  Ty::Arr(box arg,box res)
+}
+
+pub trait FTV {
+  fn contains(&self, name: TyName) -> bool;
+}
+
+impl FTV for TyName {
+  fn contains(&self, name: TyName) -> bool { 
+    *self == name 
+  }
+}
+
+impl <T:FTV> FTV for Box<T> {
+  fn contains(&self, name: TyName) -> bool {
+    self.as_ref().contains(name)
+  }
+}
+
+impl FTV for Ty {
+  fn contains(&self, name: TyName) -> bool {
+    match self {
+      Ty::V(x) => *x == name,
+      Ty::Arr(l, r) => l.contains(name) || r.contains(name),
+    }
+  }
+}
+
+#[derive(Clone,Debug)]
+pub struct TyEntry(TyName, Option<Ty>);
+
+#[macro_export]
+macro_rules! into_iter_ftv {
+  () => {
+    fn contains(&self, name: TyName) -> bool {
+      self.clone().into_iter().any(|x|x.contains(name))
+    }
+  }
+}
+
+#[macro_export]
+macro_rules! iter_ftv {
+  () => {
+    fn contains(&self, _name: TyName) -> bool {
+      panic!("wtf")
+  //    <Self as Iterator>::any(&self,|x|x.contains(name))
+    }
+  }
+}
+  
+impl <A:FTV> FTV for Option<A> {
+  into_iter_ftv!{}
+}
+impl <A:FTV> FTV for Bwd<A> { iter_ftv!{} }
+impl <A:FTV> FTV for Fwd<A> { iter_ftv!{} }
+
+impl FTV for TyEntry {
+  fn contains(&self, name: TyName) -> bool {
+    match self {
+      TyEntry(_,Some(x)) => x.contains(name),
+      TyEntry(_,None) => false
+    }
+  }
+}
+
+#[derive(Debug,Clone)]
+pub enum Entry {
+  TY(TyEntry) // | ...
+}
+
+pub type Ctx = Bwd<Entry>;
+pub type Suffix = Fwd<TyEntry>;
+
+pub fn affix(mut ctx: Ctx, mut suffix: Suffix) -> Ctx {
+  while let Some((te,new_suffix)) = suffix.uncons() {
+    ctx = ctx.snoc(Entry::TY(te));
+    suffix = new_suffix;
+  }
+  ctx
+}
+
+#[derive(Debug,Clone)]
+pub enum Ext {
+  Restore,
+  Replace(Suffix)
+}
+
+#[allow(non_upper_case_globals)]
+pub const restore : Ext = Ext::Restore;
+pub fn replace(suffix: Suffix) -> Ext { Ext::Replace(suffix) }
+
+impl Default for Ext {
+    fn default() -> Self { restore }
+}
+
+pub trait AsNoContext {
+  fn no_context() -> Self;
+}
+
+#[derive(Debug,Clone,PartialEq,Eq,PartialOrd,Ord)]
+pub enum UnifyError {
+  Occurs,
+  Mismatch(Ty,Ty),
+  NoContext
+}
+
+impl AsNoContext for UnifyError {
+  fn no_context() -> Self { UnifyError::NoContext }
+}
+
+#[derive(Debug,Clone)]
+pub struct Context {
+  next: u32,
+  ctx: Ctx
+}
+
+impl Default for Context {
+  fn default() -> Self { 
+    Context { 
+      next: 0, 
+      ctx: Bwd::nil() 
+    }
+  }
+}
+
+impl Context {
+  fn fresh (&mut self, decl: Option<Ty>) -> TyName {
+    self.next += 1;
+    let next = TyName(self.next);
+    self.ctx = self.ctx.clone().snoc(Entry::TY(TyEntry(next,decl)));
+    next
+  }
+
+  fn push(&mut self, suffix: Suffix) {
+    self.ctx = affix(self.ctx.clone(), suffix)
+  }
+
+  #[allow(non_snake_case)]
+  fn onTop<E : AsNoContext,F>(&mut self, f: F) -> Result<(),E> where
+     F : FnOnce(&mut Context,TyEntry) -> Result<Ext,E> {
+     let (new_ctx, nuD) = self.ctx.unsnoc().ok_or_else(|| E::no_context() )?;
+     self.ctx = new_ctx;
+     Ok(match nuD {
+      Entry::TY(aD) => {
+        match f(self,aD.clone())? {
+          Ext::Replace(suffix) => self.push(suffix),
+          Ext::Restore => self.ctx = self.ctx.clone().snoc(Entry::TY(aD))
+        }
+      },
+      #[allow(unreachable_patterns)] // for future expansion
+      _ => { 
+        self.onTop(f)?;
+        self.ctx = self.ctx.clone().snoc(nuD)
+      }
+    })
+  }
+
+  // TODO: borrow lhs and rhs 
+  fn unify(&mut self, lhs: &Ty, rhs: &Ty) -> Result<(),UnifyError> {
+    match (lhs,rhs) {
+      (Ty::Arr(box t0,box t1),Ty::Arr(box v0,box v1)) => { 
+        self.unify(&t0,&t1)?;
+        self.unify(&v0,&v1) 
+      },
+      (Ty::V(a), Ty::V(b)) => {
+        self.onTop(|this,te| {
+          let TyEntry(g, d) = te; 
+          Ok(
+            match (g == *a, g == *b, d) {
+              (true, true,  _) => restore,
+              (true, false, None) => replace(Fwd::cons(TyEntry(*a,Some(v(*b))),Fwd::nil())),
+              (false, true, None) => replace(Fwd::cons(TyEntry(*b,Some(v(*a))),Fwd::nil())),
+              (true, false, Some(t)) => { this.unify(&v(*b),&t)?; restore }
+              (false, true, Some(t)) => { this.unify(&v(*a),&t)?; restore }
+              (false, false, _) => { this.unify(&v(*a),&v(*b))?; restore }
+            }          
+          )
+        })
+      },
+      (Ty::V(a), t) => self.solve(*a,Fwd::nil(),t),
+      (t, Ty::V(b)) => self.solve(*b,Fwd::nil(),t)
+    }
+  }
+
+  // todo generalize error type to AsUnifyError?
+  fn solve(&mut self, a: TyName, suffix: Suffix, t: &Ty) -> Result<(),UnifyError> {
+    self.onTop(|this,te| -> Result<Ext, UnifyError> {
+      let TyEntry(g, d) = te;
+      let occurs = t.contains(g) || suffix.contains(g);
+      match (g == a, occurs, d) {
+        (true, true, _) => 
+          Err(UnifyError::Occurs),
+        (true, false, None) => 
+          // requires 
+          Ok(replace(suffix.append(&Fwd::singleton(TyEntry(a,Some(t.clone())))))),
+        (true, false, Some(v)) => {
+          this.push(suffix);
+          this.unify(&v,&t)?;
+          Ok(restore)
+        },
+        (false, true, d) => {
+          this.solve(a,Fwd::cons(TyEntry(g,d),suffix), t)?;
+          Ok(replace(Fwd::nil())) 
+        }
+        (false, false, _) => {
+          this.solve(a,suffix,t)?;
+          Ok(restore)
+        }
+      }
+    })
+  }
+}
