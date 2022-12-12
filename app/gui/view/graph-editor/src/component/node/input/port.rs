c2c9bac156d98160529e3aabf1e13ec845c7939d
Frizi <frizi09@gmail.com>
2022-12-12 15:14:58 +0100

    add tag_values to suggestion entry and propagate it to node widgets

--- app/gui/view/graph-editor/src/component/node/input/port.rs
+++ app/gui/view/graph-editor/src/component/node/input/port.rs
@@ -5,8 +5,10 @@ use enso_text::unit::*;
 use ensogl::display::shape::*;
 
 use crate::node::input::area;
+use crate::node::input::widget::NodeWidget;
 use crate::Type;
 
+use ensogl::application::Application;
 use ensogl::data::color;
 use ensogl::display;
 
@@ -102,10 +104,10 @@ impl Shape {
         let viz = viz::View::new();
 
         let width_padded = size.x + 2.0 * PADDING_X;
-        hover.size.set(Vector2::new(width_padded, hover_height));
-        viz.size.set(Vector2::new(width_padded, size.y));
-        hover.mod_position(|t| t.x = size.x / 2.0);
-        viz.mod_position(|t| t.x = size.x / 2.0);
+        hover.size.set(Vector2(width_padded, hover_height));
+        viz.size.set(Vector2(width_padded, size.y));
+        hover.set_x(size.x / 2.0);
+        viz.set_x(size.x / 2.0);
         viz.color.set(color::Rgba::transparent().into());
 
         root.add_child(&hover);
@@ -141,6 +143,7 @@ ensogl::define_endpoints! {
 
     Output {
         tp (Option<Type>),
+        new_value (String),
     }
 }
 
@@ -149,13 +152,14 @@ ensogl::define_endpoints! {
 #[derive(Clone, Debug, Default)]
 #[allow(missing_docs)]
 pub struct Model {
-    pub frp:             Frp,
-    pub shape:           Option<Shape>,
-    pub name:            Option<String>,
-    pub index:           ByteDiff,
-    pub local_index:     ByteDiff,
-    pub length:          ByteDiff,
-    pub highlight_color: color::Lcha, // TODO needed? and other fields?
+    pub frp:         Frp,
+    pub shape:       Option<Shape>,
+    pub widget:      Option<NodeWidget>,
+    // pub name:            Option<String>,
+    pub index:       ByteDiff,
+    pub local_index: ByteDiff,
+    pub length:      ByteDiff,
+    // pub highlight_color: color::Lcha, // TODO needed? and other fields?
 }
 
 impl Deref for Model {
@@ -180,6 +184,16 @@ impl Model {
         let shape = Shape::new(size, hover_height);
         self.shape = Some(shape);
         self.shape.as_ref().unwrap().clone_ref()
+    }
+
+    pub fn init_widget(
+        &mut self,
+        app: &Application,
+        argument_info: Option<span_tree::ArgumentInfo>,
+        node_height: f32,
+    ) -> Option<NodeWidget> {
+        let Some(argument_info) = argument_info else { return None };
+        NodeWidget::new(app, argument_info, node_height)
     }
 
     /// The range of this port.
